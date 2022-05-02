e6a30e411a212cafdc224f91d344cf5ddf7c9112
rhenz <salvador.rhenz@gmail.com>
2022-05-03 02:13:55 +0800

    Setup project

---
+++ Banco/Banco/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  Banco
+//
+//  Created by John Salvador on 5/3/22.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        window?.backgroundColor = .systemBackground
+        window?.rootViewController = ViewController()
+        
+        return true
+    }
+}
+
---
+++ Banco/Banco/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Banco
+//
+//  Created by John Salvador on 5/3/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
